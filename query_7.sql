75b4878a2a5c91ec36b422fb1abd0b94313349b1
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2022-09-08 10:33:35 +0530

    Update query_7.sql

--- query_7.sql
+++ query_7.sql
@@ -7,3 +7,23 @@ FROM   users
 GROUP  BY likes.user_id
 HAVING num_likes = (SELECT Count(*)
                     FROM   photos); 
+
+Output :
++--------------------+-----------+
+| username           | num_likes |
++--------------------+-----------+
+| Aniya_Hackett      |       257 |
+| Jaclyn81           |       257 |
+| Rocio33            |       257 |
+| Maxwell.Halvorson  |       257 |
+| Ollie_Ledner37     |       257 |
+| Mckenna17          |       257 |
+| Duane60            |       257 |
+| Julien_Schmidt     |       257 |
+| Mike.Auer39        |       257 |
+| Nia_Haag           |       257 |
+| Leslie67           |       257 |
+| Janelle.Nikolaus81 |       257 |
+| Bethany20          |       257 |
++--------------------+-----------+
+13 rows in set (0.02 sec)

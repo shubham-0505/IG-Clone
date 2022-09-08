943c9fa045b8be70abfa56e1cbd9090cfdb29e86
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2022-09-08 10:31:01 +0530

    Update query_5.sql

--- query_5.sql
+++ query_5.sql
@@ -2,3 +2,11 @@
 SELECT (SELECT Count(*)
         FROM   photos) / (SELECT Count(*)
                           FROM   users) AS "avg photos per user"; 
+
+Output :
++---------------------+
+| avg photos per user |
++---------------------+
+|              2.5700 |
++---------------------+
+1 row in set (0.05 sec)

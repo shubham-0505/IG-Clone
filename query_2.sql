aab49bd295d829b5a3c4fd541122c016fb89bdf6
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2022-09-08 10:24:16 +0530

    Update query_2.sql

--- query_2.sql
+++ query_2.sql
@@ -5,3 +5,12 @@ FROM   users
 GROUP  BY day
 ORDER  BY total DESC
 LIMIT  2; 
+
+Output :
++----------+-------+
+| day      | total |
++----------+-------+
+| Thursday |    16 |
+| Sunday   |    16 |
++----------+-------+
+2 rows in set (0.00 sec)

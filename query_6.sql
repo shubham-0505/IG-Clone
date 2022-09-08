8f355efdbe95e8cda39d81634ae391ce494ce61f
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2022-09-08 10:31:53 +0530

    Update query_6.sql

--- query_6.sql
+++ query_6.sql
@@ -8,3 +8,15 @@ FROM   photo_tags
 GROUP  BY photo_tags.tag_id
 ORDER  BY total DESC
 LIMIT  5; 
+
+Output :
++--------+----------+-------+
+| tag_id | tag_name | total |
++--------+----------+-------+
+|     21 | smile    |    59 |
+|     20 | beach    |    42 |
+|     17 | party    |    39 |
+|     13 | fun      |    38 |
+|     18 | concert  |    24 |
++--------+----------+-------+
+5 rows in set (0.00 sec)
